5e4ad3ef688c04bd72e04a4065bd2c405951dffd
ark02 <op@amirrk.com>
2023-04-19 11:13:34 -0700

    set pinecone to default

--- scripts/config.py
+++ scripts/config.py
@@ -81,7 +81,7 @@ class Config(metaclass=Singleton):
         self.memory_index = os.getenv("MEMORY_INDEX", 'auto-gpt')
         # Note that indexes must be created on db 0 in redis, this is not configurable.
 
-        self.memory_backend = os.getenv("MEMORY_BACKEND", 'local')
+        self.memory_backend = os.getenv("MEMORY_BACKEND", 'pinecone')
         # Initialize the OpenAI API client
         openai.api_key = self.openai_api_key
 
