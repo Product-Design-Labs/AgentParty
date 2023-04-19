2e4e1b615ef6320b4c9037bbc4bf92a6fe5811a1
ark02 <op@amirrk.com>
2023-04-19 11:14:56 -0700

    add error handling for get_ada_embedding

--- scripts/memory/base.py
+++ scripts/memory/base.py
@@ -2,15 +2,23 @@
 import abc
 from config import AbstractSingleton, Config
 import openai
+import time
+from openai.error import APIError
 cfg = Config()
 
 
-def get_ada_embedding(text):
-    text = text.replace("\n", " ")
-    if cfg.use_azure:
-        return openai.Embedding.create(input=[text], engine=cfg.azure_embeddigs_deployment_id, model="text-embedding-ada-002")["data"][0]["embedding"]
-    else:
-        return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+
+def get_ada_embedding(text: str) -> list:
+    retries = 3
+    for i in range(retries):
+        try:
+            return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+        except APIError as e:
+            if i < retries - 1:
+                print(f"Error occurred while fetching embedding. Retrying {i + 1}/{retries}...")
+                time.sleep(2 ** i)  # Exponential backoff
+            else:
+                raise e
 
 
 class MemoryProviderSingleton(AbstractSingleton):
